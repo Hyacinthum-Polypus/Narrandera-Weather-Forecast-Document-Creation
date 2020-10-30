319528357499932d9bfae18c6a4a10ca477d9280
William Rivero <williamrivero@pm.me>
2020-10-30 16:52:37 +1100

    Hidden bullet styles, added fix images function.

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,10 @@ require 'Nokogiri'
 require 'open-uri'
 require 'active_support/core_ext/hash'
 
+def FixImages(body, url)
+  return body = body.gsub(/src../, " src=\"#{url}")
+end
+
 url = 'https://www.eldersweather.com.au/local-forecast/nsw/narrandera'
 html = URI.open(url)
 doc = Nokogiri::HTML(html)
@@ -9,9 +13,10 @@ doc = Nokogiri::HTML(html)
 sevenDayForecast = doc.css("li.seven-day-forecast.full")
 sunRiseSunSet = doc.css("li.sunrise-sunset.full")
 
-content = "<html><head><style type=\"text/css\">table, th, td {border: 1px solid black;}</style></head><body>" + sevenDayForecast.to_s() + sunRiseSunSet.to_s()  + "</body></html>"
+content = "<html><head><style type=\"text/css\">table, th, td {border: 1px solid black;} li { list-style-type: none;}</style></head><body>" + sevenDayForecast.to_s() + sunRiseSunSet.to_s()  + "</body></html>"
+
+content = FixImages(content, "https://www.eldersweather.com.au")
 
 puts content
 
 File.open("Forecast.html", "w") { |f| f.write "#{content}"}
-File.close
