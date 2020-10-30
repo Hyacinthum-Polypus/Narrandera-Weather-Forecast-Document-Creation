36e59b12b717c35735053b39417b48c438e8cb91
William Rivero <williamrivero@pm.me>
2020-10-31 09:46:46 +1100

    Remove List Items

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,15 @@
 require 'Nokogiri'
 require 'open-uri'
 
+
+
 def FixImages(body, url)
   return body = body.gsub(/src../, " src=\"#{url}")
+end
+
+def RemoveListItems(body)
+  body = body.gsub(/<li([^>])+>/, "")
+  return body.gsub(/<\/li>/, "")
 end
 
 url = 'https://www.eldersweather.com.au/local-forecast/nsw/narrandera'
@@ -12,9 +19,11 @@ doc = Nokogiri::HTML(html)
 sevenDayForecast = doc.css("li.seven-day-forecast.full")
 sunRiseSunSet = doc.css("li.sunrise-sunset.full")
 
-content = "<html><head>  <style type=\"text/css\">table, th, td {border: 1px solid black;} li { list-style-type: none;}</style> </head><body>" + sevenDayForecast.to_s() + sunRiseSunSet.to_s()  + "</body></html>"
+content = "<html><head>  <style type=\"text/css\">table, th, td {border: 1px solid black;} </style> </head><body>" + sevenDayForecast.to_s() + sunRiseSunSet.to_s()  + "</body></html>"
 
 content = FixImages(content, "https://www.eldersweather.com.au")
+
+content = RemoveListItems(content)
 
 puts content
 
